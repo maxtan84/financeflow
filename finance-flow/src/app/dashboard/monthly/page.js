dcd844d7d0e2242c6e0c82336f10fd11f1fd0a8c
maxtan84 <mthockey84@gmail.com>
2023-06-22 19:53:22 -0400

    saving changes

--- finance-flow/src/app/dashboard/monthly/page.js
+++ finance-flow/src/app/dashboard/monthly/page.js
@@ -15,6 +15,7 @@ export default function Monthly() {
             <DashHeader className="self-start" title="Monthly Spendings" />
             <div className="flex-grow">
                 <Month month="January" data={data} className=""/>
+                <Month month="Febuary" data={data} className=""/>
             </div>
             <DashFooter className="self-end mt-auto" curFocus={"calendar"} />
         </div>
@@ -23,7 +24,7 @@ export default function Monthly() {
 
 const Month = ({ month, data }) => {
     return (
-        <div className="h-[10%]">
+        <div className="h-[10%] m-8">
             <h1>{month}</h1>
             <PieChart data={data} />
         </div>
--- finance-flow/src/app/dashboard/page.js
+++ finance-flow/src/app/dashboard/page.js
@@ -4,7 +4,7 @@ import DashHeader from "../components/dash-header";
 export default function Dashboard() {
   return (
     <div className="flex flex-col h-screen">
-      <DashHeader className="self-start" title="Main DashBoard" />
+      <DashHeader className="self-start" title="DashBoard" />
       <div className="flex-grow"></div> {/* content */}
       <DashFooter className="self-end mt-auto" curFocus={"dash"}/>
     </div>
