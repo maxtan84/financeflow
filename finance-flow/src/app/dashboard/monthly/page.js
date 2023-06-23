1b3d66eea37d38caf0afb41d2289de8c1b6d9927
maxtan84 <mthockey84@gmail.com>
2023-06-23 19:59:58 -0400

    transactions are displayed

--- finance-flow/src/app/dashboard/monthly/page.js
+++ finance-flow/src/app/dashboard/monthly/page.js
@@ -1,6 +1,11 @@
+'use client'
 import DashFooter from "@/app/components/dash-footer"
 import DashHeader from "@/app/components/dash-header"
 import PieChart from "@/app/components/PieChart"
+import { useState } from "react"
+import firebase from "firebase/compat/app"
+import 'firebase/compat/firestore'
+import { useEffect } from "react"
 
 export default function Monthly() {
 
@@ -15,7 +20,6 @@ export default function Monthly() {
             <DashHeader className="self-start" title="Monthly Spendings" />
             <div className="flex-grow">
                 <Month month="January" data={data} className=""/>
-                <Month month="Febuary" data={data} className=""/>
             </div>
             <DashFooter className="self-end mt-auto" curFocus={"calendar"} />
         </div>
@@ -23,10 +27,48 @@ export default function Monthly() {
 }
 
 const Month = ({ month, data }) => {
+
+    const userId = localStorage.getItem("userId");
+    const [transactions, setTransactions] = useState([]);
+
+    useEffect(() => {
+        const getTransactions = () => {
+          const db = firebase.firestore();
+          db.collection('transactions')
+            .where('userId', '==', userId)
+            .get()
+            .then((querySnapshot) => {
+              const data = querySnapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+              }));
+              setTransactions(data);
+            })
+            .catch((error) => {
+              console.error('Error fetching transactions:', error);
+            });
+        };
+        getTransactions();
+    }, [userId]);
+
+
     return (
         <div className="h-[10%] m-8">
             <h1>{month}</h1>
             <PieChart data={data} />
+            {transactions.length > 0 ? (
+            <ul>
+            {transactions.map((transaction) => (
+                <li key={transaction.id}>
+                <p>Name: {transaction.name}</p>
+                <p>Amount: {transaction.amount}</p>
+                <p>Date: {transaction.date}</p>
+                </li>
+                ))}
+            </ul>
+            ) : (
+                <p>No transactions found for {month}</p>
+            )}
         </div>
     )
 }   
